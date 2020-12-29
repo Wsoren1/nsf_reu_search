159688aaaac64d21ad0b463e2ca37743e4fb7124
wsoren1 <wsorensen@umass.edu>
2020-12-29 13:51:45 -0500

    individual keywords now should work, unit tests needed

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import keyboard
 from search_keywords import database
 
 
-nsf_reus = database('search_results.csv')
+nsf_reus = database('search_results.csv', 'keywords.data')
 
 print('Up == Yes, Down == No')
-for keyword in nsf_reus.get_unique_keywords():
-    print(f'Keep keywords {keyword}?')
+for keyword in nsf_reus.uniques:
+    print(f'Keep keyword {keyword}?')
     while True:
         sleep(0.05)
-
-        if keyboard.is_pressed('down'):
+        pre_len = nsf_reus.df.shape[0]
+        if keyboard.is_pressed('left'):
             sleep(0.75)
             nsf_reus.elim_keyword(keyword)
             break
 
-        if keyboard.is_pressed('up'):
+        if keyboard.is_pressed('right'):
             sleep(0.75)
             break
 
         if keyboard.is_pressed('escape'):
             nsf_reus.save()
             exit()
-
+        if pre_len != nsf_reus.df.shape[0]:
+            print(f'Reduced {pre_len} entries to {nsf_reus.df.shape[0]}')
 nsf_reus.save()
--- search_keywords.py
+++ search_keywords.py
@@ -1,16 +1,25 @@
 import pandas as pd
+import re
+import pickle
 
 
 class database:
-    def __init__(self, fpath):
+    def __init__(self, fpath, keywords_fpath=None):
         self.df = pd.read_csv(fpath)
         self.df['Research Topics/Keywords'] = self.df['Research Topics/Keywords'].apply(lambda x: x.lower())
+        if keywords_fpath is None:
+            self.uniques = self.get_unique_keywords()
+        else:
+            with open('keywords.data', 'rb') as f:
+                self.uniques = pickle.load(f)
+
 
     def get_unique_keywords(self):
         _uniques = []
-        for keyword in self.df['Research Topics/Keywords']:
-            if keyword not in _uniques:
-                _uniques.append(keyword)
+        for entry in self.df['Research Topics/Keywords']:
+            for keyword in entry.split(','):
+                if keyword not in _uniques:
+                    _uniques.append(keyword)
         return _uniques
 
     @staticmethod
@@ -23,18 +32,24 @@ class database:
                     return True
         return False
 
-    def elim_keyword(self, keyword): # => dataframe
+    def elim_keyword(self, keyword):  # => dataframe
         keyword = keyword.lower()
 
-        pre_query_len = self.df.shape[0]
+        if keyword not in self.uniques:
+            print(f'{keyword} not found')
+            return self.df
 
-        filt = self.df['Research Topics/Keywords'].str.contains(keyword)
-        result = self.df[~filt]
+        self.uniques.remove(keyword)
 
-        if pre_query_len == result.shape[0]:
-            print(f'{keyword} not found in keywords')
+        reg_search = re.escape('|'.join(self.uniques).replace('||', '|'))
+        # df["TrueFalse"] = df['col1'].apply(lambda x: 1 if any(i in x for i in searchfor) else 0)
+        filt = self.df['Research Topics/Keywords'].apply(lambda x: True if any(i in x for i in self.uniques) else False)
+        result = self.df[filt]
+
         self.df = result
         return result
 
     def save(self):
+        with open('keywords.data', 'wb') as f:
+            pickle.dump(self.uniques, f)
         self.df.to_csv('search_results.csv', index=False)
